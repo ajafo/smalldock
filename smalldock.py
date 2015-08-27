ecea38440170d8fe6e92dcba315ff77a051758a2
ajafo <github@ajafo.com>
2015-08-27 17:33:45 +0200

    unknown instance bugfix

--- smalldock.py
+++ smalldock.py
@@ -33,33 +33,39 @@ def print_config():
      print dock.containers()
 
 
+
+
 def startInstance(inst,id,inst_ver):
      print inst
-     cnf=dock.inspect_container(id)
-     ip_wew=cnf['NetworkSettings']['IPAddress']
-     conf.li[inst].showIntIp()
-     conf.li[inst].addIntIp(id,ip_wew)
-
-     if conf.li[inst].ip_pool[0] is not "0":
-         ip_zew=conf.li[inst].getFreeIp()
-         print 'Assign external IP: ' + ip_zew
-         executor.addIpRule(ip_wew,ip_zew)
-         conf.li[inst].addExtIp(id,ip_zew)
+     if inst in conf.li.keys():
+         cnf=dock.inspect_container(id)
+         ip_wew=cnf['NetworkSettings']['IPAddress']
+         conf.li[inst].showIntIp()
+         conf.li[inst].addIntIp(id,ip_wew)
+         if conf.li[inst].ip_pool[0] is not "0":
+              ip_zew=conf.li[inst].getFreeIp()
+              print 'Assign external IP: ' + ip_zew
+              executor.addIpRule(ip_wew,ip_zew)
+              conf.li[inst].addExtIp(id,ip_zew)
+         else:
+             print "This machine have no IP assigned"
      else:
-         print "This machine have no IP assigned"
-
+         print "There is no configuration for: " + inst
 
 def stopInstance(inst,id,version):
-     ip_wew=conf.li[inst].getIntIP(id)
-     print ip_wew
-     conf.li[inst].removeIntIp(id)
-     if conf.li[inst].ip_pool[0] is not "0":
-         ip_zew=conf.li[inst].getExtIp(id)
-         print 'Deassign external IP: ' + ip_zew
-         executor.delIpRule(ip_wew,ip_zew)
-         conf.li[inst].removeExtIp(id)
+     if inst in conf.li.keys():
+         ip_wew=conf.li[inst].getIntIP(id)
+         print ip_wew
+         conf.li[inst].removeIntIp(id)
+         if conf.li[inst].ip_pool[0] is not "0":
+             ip_zew=conf.li[inst].getExtIp(id)
+             print 'Deassign external IP: ' + ip_zew
+             executor.delIpRule(ip_wew,ip_zew)
+             conf.li[inst].removeExtIp(id)
+         else:
+             print "This machine have no IP to remove "
      else:
-         print "This machine have no IP to remove "
+         print "There is no configuration for: " + inst
 
 
 
@@ -71,12 +77,14 @@ def stopInstance(inst,id,version):
 
 
 def runInstances(inst,ver,count):
-    print 'Start instance ' + inst + ":" + ver, count
-    for c in range(0,int(count)):
-        container = dock.create_container(image=inst + ":" + ver)
-        response = dock.start(container=container.get('Id'))
-        print response
-
+    if inst in conf.li.keys():
+        print 'Start instance ' + inst + ":" + ver, count
+        for c in range(0,int(count)):
+            container = dock.create_container(image=inst + ":" + ver)
+            response = dock.start(container=container.get('Id'))
+            print response
+    else:
+        print "There is no configuration for: " + inst
 
 
 
@@ -105,27 +113,28 @@ executor.nginxRestart()
 
 for line in dock.events():
      d=json.loads(line)
-
      instli=d["from"].split(':')
      inst_name=instli[0]
      inst_ver=instli[1]
-     upstream_file=conf.li[inst_name].getUpFile()
-
-     print upstream_file
-     print inst_name
-     print inst_ver
 
      if d["status"] == "start":
-          print "start action"
-          startInstance(inst_name,d["id"],inst_ver)
-          conf.show_config(inst_name,inst_ver)
-          conf.generate_upstrean(inst_name,d["id"],inst_ver,upstream_file)
-          executor.nginxRestart()
+          if inst_name in conf.li.keys():
+              print "start action"
+              upstream_file=conf.li[inst_name].getUpFile()
+              startInstance(inst_name,d["id"],inst_ver)
+              conf.show_config(inst_name,inst_ver)
+              conf.generate_upstrean(inst_name,d["id"],inst_ver,upstream_file)
+              executor.nginxRestart()
+          else:
+              print "There is no configuration for: " + inst_name + ":" + inst_ver
      elif d["status"] == "die":
-          print "die action"
-          stopInstance(inst_name,d["id"],inst_ver)
-          conf.show_config(inst_name,inst_ver)
-          conf.generate_upstrean(inst_name,d["id"],inst_ver,upstream_file)
-          executor.nginxRestart()
+          if inst_name in conf.li.keys():
+              print "die action"
+              stopInstance(inst_name,d["id"],inst_ver)
+              conf.show_config(inst_name,inst_ver)
+              conf.generate_upstrean(inst_name,d["id"],inst_ver,upstream_file)
+              executor.nginxRestart()
+          else:
+              print "There is no configuration for: " + inst_name + ":" + inst_ver
 
 
